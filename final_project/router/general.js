002e9ed6db72e4f3b16d9c5ba04df1acde147212
Erick Rivera <rivera.erick95@gmail.com>
2023-03-11 05:27:27 -0500

    Tasks 1 through five completed
    Signed-off-by: Erick Rivera <rivera.erick95@gmail.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,10 +9,10 @@ public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-  if (!username && !password){
-      res.status(404).json({message:""})
-  }
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (!username || !password){
+        return res.status(404).json({message:""})
+    }
+    return res.status(200).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
@@ -22,20 +22,19 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    let filtered_books = books.filter((book) => book.isbn === isbn);
-    if (filtered_books.length == 0) {
-        res.status(204).json({message: `Book with ISBN: ${isbn} not found.`});
+    let isbn = req.params.isbn;
+    if (!books.hasOwnProperty(isbn)) {
+        return res.status(404).json({message: `Book match with ISBN: ${isbn} not found.`});
     }
-  return res.status(200).send(JSON.stringify(filtered_books[0]));
+    return res.status(200).send(JSON.stringify({[isbn] : books[isbn]}));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    let filtered_books = books.filter((book) => book.author.includes(author));
+    let filtered_books = books.entries().filter((book) => book.author.includes(author));
     if (filtered_books.length == 0) {
-        res.status(204).json({message: `No matches for books with author: ${author}.`});
+        return res.status(404).json({message: `No matches for books with author: ${author}.`});
     }
   return res.status(200).send(JSON.stringify(filtered_books));
 });
@@ -43,9 +42,11 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    let filtered_books = books.filter((book) => book.title.includes(title));
-    if (filtered_books.length == 0) {
-        res.status(204).json({message: `No matches for books with title: ${title}.`});
+    let booksArray = Object.entries(books);
+    let filtered_books = booksArray.filter(([isbn, book]) => {
+    book.title.includes(title);});
+    if (filtered_books.length === 0) {
+        return res.status(404).json({message: `No matches for books with title: ${title}.`});
     }
   return res.status(200).send(JSON.stringify(filtered_books));
 });
@@ -53,11 +54,10 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    let filtered_books = books.filter((book) => book.isbn === isbn);
-    if (filtered_books.length == 0) {
-        res.status(204).json({message: `Book matches with ISBN: ${isbn} not found.`});
+    if (!books.hasOwnProperty(isbn)) {
+        return res.status(404).json({message: `Book match with ISBN: ${isbn} not found.`});
     }
-  return res.status(200).send(JSON.stringify(filtered_books[0]));
+    return res.status(200).send(JSON.stringify({[isbn] : books[isbn]}));
 });
 
 module.exports.general = public_users;
